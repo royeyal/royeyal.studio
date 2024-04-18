09fd3d881f5f69dad812786194363590c1a58a22
Roy Eyal <roy.eyal@gmail.com>
2024-04-18 12:55:22 +0300

    Update royeyal.studio.js

--- royeyal.studio.js
+++ royeyal.studio.js
@@ -1,24 +1,39 @@
+/****
+ * Load first: GSAP, ScrollTrigger, SplitText
+ *****/
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function setupSplits() {
+	const targets = gsap.utils.toArray(".heading-style-h1, .heading-style-h2");
+	targets.forEach((target) => {
+		let splitHeading = new SplitText(target, {
+			type: "words, chars",
+		});
+		let chars = splitHeading.chars; //an array of all the divs that wrap each character
+		gsap.from(chars, {
+			yPercent: 130,
+			stagger: 0.05,
+			ease: "back.out",
+			duration: 0.7,
+			scrollTrigger: {
+				trigger: target,
+				//markers: true,
+				start: "top 75%",
+				end: "bottom center",
+				//scrub: true
+			},
+		});
+	});
+}
+setupSplits();
+
 /***************************/
 /* Cuberto Mouse Follower */
 /*************************/
 // Initialize a new MouseFollower instance with custom settings
+// var cursor = new MouseFollower();
 const cursor = new MouseFollower({
 	speed: 0.4, // Cursor movement speed
 	skewing: 1, // Skewing effect for the cursor
 	skewingText: 3, // Skewing effect specifically for the text in the cursor
-});
-// Select all elements with the 'product-image' class
-const el = document.querySelectorAll(".grid-item-image-wrapper");
-el.forEach(function (element) {
-	// When the mouse enters the element, set the cursor text to 'View'
-	element.addEventListener("mouseenter", () => {
-		cursor.show();
-		cursor.setText("View");
-	});
-
-	// When the mouse leaves the element, remove the text from the cursor
-	element.addEventListener("mouseleave", () => {
-		cursor.hide();
-		cursor.removeText();
-	});
 });
