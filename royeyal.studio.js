9a65a2bd9cd84e53bc2c022ec655e23f4ec04bfd
Roy Eyal <roy.eyal@gmail.com>
2024-04-18 12:49:06 +0300

    Update royeyal.studio.js

--- royeyal.studio.js
+++ royeyal.studio.js
@@ -7,3 +7,18 @@ const cursor = new MouseFollower({
 	skewing: 1, // Skewing effect for the cursor
 	skewingText: 3, // Skewing effect specifically for the text in the cursor
 });
+// Select all elements with the 'product-image' class
+const el = document.querySelectorAll(".grid-item-image-wrapper");
+el.forEach(function (element) {
+	// When the mouse enters the element, set the cursor text to 'View'
+	element.addEventListener("mouseenter", () => {
+		cursor.show();
+		cursor.setText("View");
+	});
+
+	// When the mouse leaves the element, remove the text from the cursor
+	element.addEventListener("mouseleave", () => {
+		cursor.hide();
+		cursor.removeText();
+	});
+});
