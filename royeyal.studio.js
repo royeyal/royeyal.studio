db14759b4d2a52a0a24a57edff1842b18b4eeb21
Roy Eyal <roy.eyal@gmail.com>
2024-05-27 22:13:51 +0300

    Added text fade

--- royeyal.studio.js
+++ royeyal.studio.js
@@ -43,6 +43,34 @@ setupSplits();
 // 	stagger: 0.1,
 // });
 
+// ScrollTrigger.batch(".w-richtext p", {
+// 	onEnter: elements => {
+// 	  gsap.from(elements, {
+// 		autoAlpha: 0,
+// 		y: 60,
+// 		stagger: 0.15,
+// 		ease: "power4.inOut",
+// 	  });
+// 	},
+// 	once: true
+//   });
+
+gsap.utils.toArray('.w-richtext p, .w-richtext h2, .w-richtext h3, .w-richtext li').forEach(el => {
+	gsap.from(el, {
+	  opacity: 0, // Start with opacity 0
+	  duration: 1, // Animation duration
+	  scrollTrigger: {
+		trigger: el, // Element to trigger the animation
+		start: "top 80%", // Start animation when element is 80% in view
+		end: "bottom 20%", // End animation when element is 20% out of view
+		toggleActions: "play none none reverse", // Play animation on enter, reverse on exit
+		stagger: 0.15,
+      	ease: "power4.inOut",
+	  },
+	});
+  });
+  
+
 /***************************/
 /* Cuberto Mouse Follower */
 /*************************/
